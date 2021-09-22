1179b4513bf92e0f27236afab4a933ac201a750f
suryaprakashramaraj <711suryaprakash@gmail.com>
2021-09-22 11:59:32 +0530

    new task added

---
+++ javaReposoteries.java
@@ -0,0 +1,19 @@
+package org.sampleJava;
+
+public class javaReposoteries {
+		  
+		private void tc1() {
+			System.out.println(1);
+			
+		}
+		private void tc2() {
+			System.out.println("browser launch");
+		}
+		
+		public static void main(String[] args) {
+			javaReposoteries r = new javaReposoteries();
+			
+			r.tc1();
+			r.tc2();
+		}
+}
