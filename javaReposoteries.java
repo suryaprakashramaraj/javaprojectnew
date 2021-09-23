e9b4eb2f6dadd767ee29c7d4d2a73ea030bf9c95
suryaprakashramaraj <711suryaprakash@gmail.com>
2021-09-23 13:05:13 +0530

    justupdated

--- javaReposoteries.java
+++ javaReposoteries.java
@@ -1,19 +1,25 @@
 package org.sampleJava;
 
 public class javaReposoteries {
-		  
-		private void tc1() {
-			System.out.println(1);
-			
+
+	private void tc1() {
+		System.out.println(1);
+
+	}
+
+	private void tc2() {
+		System.out.println("browser cancelled");
+	}
+
+	private void tc3() {
+		System.out.println(""asdfdfgh);
 		}
-		private void tc2() {
-			System.out.println("browser launch");
-		}
+
+	public static void main(String[] args) {
+		javaReposoteries r = new javaReposoteries();
+
+		r.tc1();
+		r.tc2();
 		
-		public static void main(String[] args) {
-			javaReposoteries r = new javaReposoteries();
-			
-			r.tc1();
-			r.tc2();
-		}
+	}
 }
